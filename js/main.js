4ab06e786b19f45a707e1884002525c61f36706d
TheLegendOfGerk <gerkiidx@gmail.com>
2022-06-28 20:51:55 -0700

    testing for class

--- js/main.js
+++ js/main.js
@@ -7,16 +7,24 @@ this is a multi-line comment, open with slash asterisk, end with asterisk slash
 // variables
 yes = true; //boolean
 no = false;
-Yes = 10;
-console.log(yes, Yes);
+Yes = 10; // case sensitivity, different from yes, yeS, YES, et cetera
+
+
+myname = 'Gerk';
+
+favfood = 'Pizza';
 
 count = 0; // number 
 one = 1;
 two = 2;
 three = 3;
 
+console.log(yes, Yes, one, two);
 
 title = 'Hello World'; // string
+
+console.log(myname, favfood);
+
 
 document.querySelector('h1').innerHTML = title;
 document.querySelector('p').innerHTML = count;
