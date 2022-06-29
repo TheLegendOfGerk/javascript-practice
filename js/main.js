128c11738c16a3aa430e6845873a1f705513f62b
TheLegendOfGerk <gerkiidx@gmail.com>
2022-06-28 20:32:04 -0700

    new for js practice

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// comment: one line comment
+
+/*
+this is a multi-line comment, open with slash asterisk, end with asterisk slash
+*/ 
+
+// variables
+yes = true; //boolean
+no = false;
+Yes = 10;
+console.log(yes, Yes);
+
+count = 0; // number 
+one = 1;
+two = 2;
+three = 3;
+
+
+title = 'Hello World'; // string
+
+document.querySelector('h1').innerHTML = title;
+document.querySelector('p').innerHTML = count;
