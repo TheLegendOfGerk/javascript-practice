acf733684ce296eef4314d492256be08ea8e6f46
TheLegendOfGerk <gerkiidx@gmail.com>
2022-06-30 20:57:40 -0700

    finishing up on Day 2

--- day02/js/main.js
+++ day02/js/main.js
@@ -46,3 +46,43 @@ document.querySelector('p').innerHTML = count;
   //array, ['like this', 'this', 'this'] is for multiples! (cue Gradius III announcer)
   //{groups}
   //array = [1, 2, 3, 4, 5] | list = [1, 2, 3, 4, 5]
+
+// operators!
+// = is assignment
+// += addition assignment
+
+/* Arithmetic examples
+1 + 1
+1 - 1
+1 * 1
+1 / 1
+1 % 1
+*/
+
+let score = 10
+
+console.log(score + 6);
+
+console.log("hello " + "world");
+console.log('don\'t do it!');
+
+console.log('It looks like \\ but I\'m actually typing \\\\.');
+
+console.log('5px' + '5px'); //aw rats, didn't work
+
+let Fiven = 5n; //This "n" determines this will be a numeric integer, instead of worrying about decimals. Javascript can go to 19 decimal places normally.
+
+console.log(Fiven / 3n);
+
+const unique1 = Symbol('unique');
+const unique2 = Symbol('unique');
+
+console.log(unique2 === unique1, 'unique'); // unique symbols, even though they *appear* the same, do not actually equal one another. Symbol is the unique identifier
+
+console.log('Your score is ' + score + '. That\'s the highest yet!');
+
+console.log(`My score is ${score}`); //use tildes for "advanced strings"!
+
+console.log(gradiusThree.year);
+
+console.log(Supercalifragelisticexpialadocious.length)
