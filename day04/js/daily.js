7ef33534f83d77765321e416996ac48c13e48326
TheLegendOfGerk <gerkiidx@gmail.com>
2022-07-12 20:06:16 -0700

    day05 shenanigans

--- day04/js/daily.js
+++ day04/js/daily.js
@@ -1,12 +1,19 @@
 let firstNum = parseInt(prompt('First number?'));
 let secondNum = parseInt(prompt('Second number?'));
-
-if (firstNum > secondNum){
-    alert(`The first number, ${firstNum}, is larger!`);}
-    else {
-        if (firstNum === secondNum){
-            alert('The two numbers are the same! They\'re equal! YOU FOOL!!!!!!1')
+console.log(firstNum, secondNum)
+if (isNaN(firstNum || secondNum)){
+    alert('Oops, we didn\'t recognize your input! Be sure to only use numeric characters. e.g. 1234.');
+    reload();
+}
+else {
+    if (firstNum > secondNum){
+        alert(`The first number, ${firstNum}, is larger!`);}
+        else {
+            if (firstNum === secondNum){
+                alert('The two numbers are the same! They\'re equal! YOU FOOL!!!!!!1')
+            }
+        else {
+            alert(`The second number, ${secondNum}, is larger!`)
         }
-    else {
-        alert(`The second number, ${secondNum}, is larger!`)
-    }}
+        }
+    }
---
+++ day05/js/takehome.js
@@ -0,0 +1,6 @@
+var FirstName = prompt('What is your first name?');
+var RevName = FirstName.split('').reverse().join('');
+alertasdf(`Your secret codename is ${RevName}!`)
+
+
+//alert
